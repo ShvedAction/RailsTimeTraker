daad87b962e1d49f491b7cd9737b1397cad0023e
Aleksey Shvedujk <shvedujk@gmail.com>
2016-11-17 11:31:45 +0300

    user model implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
+require 'securerandom'
+
 class User < ApplicationRecord
-
+  enum auth_type: [:registred, :temporary]
+  validates :password, confirmation: true, if: :registred?
+  after_initialize :set_default_values
+  before_save :genrate_unic_login, if: :temporary?
+  
   attr_accessor :password_confirmation
-
+  
+  def set_default_values
+    self.auth_type ||= 'registred'
+  end
+  
+  def genrate_unic_login 
+    self.login ||= SecureRandom.uuid.gsub('-','')
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,11 +32,31 @@ class UserTest < ActiveSupport::TestCase
     new_user = User.new login: login, password: password, password_confirmation: password
     
     #When:
-    assert_difference 'User.count', "User with correct confirmation password should saved." do
+    assert_difference 'User.count', 1, "User with correct confirmation password should saved." do
       new_user.save
     end
     
     #Then:
     assert !!User.find_by(login: login), "User with correct confirmation password should found."
   end
+  
+  test "should save without password if his auth_type is temp_user" do
+  
+    temp_user = User.new auth_type: 'temporary'
+    
+    #When:
+    assert_difference 'User.count', 1, "Temp user should saved." do
+      temp_user.save
+    end
+    
+    #Then:
+    refute_empty temp_user.login, "Temp user should have unic login"
+  end
+  
+  test "on new User should auth_type init with deffault temporary" do
+    new_user = User.new
+    
+    assert new_user.registred?, "default auth_type should be registred"
+  end
+  
 end
